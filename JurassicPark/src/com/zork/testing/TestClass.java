44bd53c7d1788092756aa7ff36a1eb72c2220445
jnarayanbvg <38253333+jnarayanbvg@users.noreply.github.com>
2019-05-06 13:48:24 -0400

    Uploaded test file

---
+++ JurassicPark/src/com/zork/testing/TestClass.java
@@ -0,0 +1,7 @@
+package com.zork.testing;
+
+public class TestClass {
+	public static void main(String[] args) {
+		System.out.println("Hello!");
+	}
+}
---
+++ JurassicPark/src/module-info.java
@@ -0,0 +1,2 @@
+module Zork2019JurassicPark {
+}
