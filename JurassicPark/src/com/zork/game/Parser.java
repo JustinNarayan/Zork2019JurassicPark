66537951f716a9c94953d2bfc1421da97088d532
zvelshi <46686793+zvelshi@users.noreply.github.com>
2019-05-08 15:33:40 -0400

    nothing changed - ctrl+a and ctrl+shift+f

--- JurassicPark/src/com/zork/game/Parser.java
+++ JurassicPark/src/com/zork/game/Parser.java
@@ -1,4 +1,5 @@
 package com.zork.game;
+
 /*
  * Author:  Michael Kolling
  * Version: 1.0
@@ -18,50 +19,47 @@ package com.zork.game;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
-class Parser 
-{
-private CommandWords commands;  // holds all valid command words
-public Parser() 
-    {
-        commands = new CommandWords();
-    }
-public Command getCommand() 
-    {
-        String inputLine = "";   // will hold the full input line
-        String word1;
-        String word2;
-System.out.print("> ");     // print prompt
-BufferedReader reader = 
-            new BufferedReader(new InputStreamReader(System.in));
-        try {
-            inputLine = reader.readLine();
-        }
-        catch(java.io.IOException exc) {
-            System.out.println ("There was an error during reading: "
-                                + exc.getMessage());
-        }
-StringTokenizer tokenizer = new StringTokenizer(inputLine);
-if(tokenizer.hasMoreTokens())
-            word1 = tokenizer.nextToken();      // get first word
-        else
-            word1 = null;
-        if(tokenizer.hasMoreTokens())
-            word2 = tokenizer.nextToken();      // get second word
-        else
-            word2 = null;
+
+class Parser {
+	private CommandWords commands; // holds all valid command words
+
+	public Parser() {
+		commands = new CommandWords();
+	}
+
+	public Command getCommand() {
+		String inputLine = ""; // will hold the full input line
+		String word1;
+		String word2;
+		System.out.print("> "); // print prompt
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		try {
+			inputLine = reader.readLine();
+		} catch (java.io.IOException exc) {
+			System.out.println("There was an error during reading: " + exc.getMessage());
+		}
+		StringTokenizer tokenizer = new StringTokenizer(inputLine);
+		if (tokenizer.hasMoreTokens())
+			word1 = tokenizer.nextToken(); // get first word
+		else
+			word1 = null;
+		if (tokenizer.hasMoreTokens())
+			word2 = tokenizer.nextToken(); // get second word
+		else
+			word2 = null;
 // note: we just ignore the rest of the input line.
 // Now check whether this word is known. If so, create a command
-        // with it. If not, create a "nil" command (for unknown command).
-if(commands.isCommand(word1))
-            return new Command(word1, word2);
-        else
-            return new Command(null, word2);
-    }
-/**
-     * Print out a list of valid command words.
-     */
-    public void showCommands()
-    {
-        commands.showAll();
-    }
+		// with it. If not, create a "nil" command (for unknown command).
+		if (commands.isCommand(word1))
+			return new Command(word1, word2);
+		else
+			return new Command(null, word2);
+	}
+
+	/**
+	 * Print out a list of valid command words.
+	 */
+	public void showCommands() {
+		commands.showAll();
+	}
 }
