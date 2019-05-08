84f30de27fe15997917a676c1d104e4af52c4b03
zvelshi <46686793+zvelshi@users.noreply.github.com>
2019-05-08 19:57:39 -0400

    testing mp3 files

--- JurassicPark/src/com/zork/testing/TestClass.java
+++ JurassicPark/src/com/zork/testing/TestClass.java
@@ -1,5 +1,17 @@
 package com.zork.testing;
 
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javazoom.jl.player.Player; 
+import sun.audio.AudioData;
+import sun.audio.AudioPlayer;
+import sun.audio.AudioStream;
+import sun.audio.ContinuousAudioDataStream;
+
 public class TestClass {
 	public static void main(String[] args) {
 		System.out.println("Hello Zac!");
@@ -7,5 +19,26 @@ public class TestClass {
 		System.out.print("hi");
 		System.out.println("tada");
 		System.out.println("DO NOT PUSH YOUR settings file.");
+		System.out.println();
+		
+		play();
 	}
+	
+   public static void play() {
+   	String filename = "C:\\Users\\zacve\\git\\Zork2019JurassicPark\\JurassicPark\\src\\com\\zork\\testing\\Jurassic Park.mp3";
+   	try {
+       	
+           FileInputStream fis     = new FileInputStream(filename);
+           BufferedInputStream bis = new BufferedInputStream(fis);
+           Player player = new Player(bis);
+           player.play();
+       }
+       catch (Exception e) {
+           System.out.println("Problem playing file " + filename);
+           System.out.println(e);
+       }
+   }
+
+
+
 }
