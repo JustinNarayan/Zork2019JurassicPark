a05d1c22fc7782ea6c27d8940424425ac9f0bb0a
zvelshi <46686793+zvelshi@users.noreply.github.com>
2019-05-21 15:24:16 -0400

    added initialiser for items in rooms

---
+++ JurassicPark/src/com/zork/game/RoomItemInit.java
@@ -0,0 +1,112 @@
+package com.zork.game;
+
+import java.util.ArrayList;
+
+public class RoomItemInit {
+
+	private ArrayList<String> roomsWithTrees;
+	private ArrayList<String> roomsWithWater;
+	
+	public RoomItemInit() {
+		roomsWithTrees = new ArrayList<String>();
+		
+		// All hallways/pathways have trees in them
+		roomsWithTrees.add("HALLWAY_1");
+		roomsWithTrees.add("HALLWAY_2");
+		roomsWithTrees.add("HALLWAY_3");
+		roomsWithTrees.add("HALLWAY_4");
+		roomsWithTrees.add("HALLWAY_5");
+		roomsWithTrees.add("HALLWAY_6");
+		roomsWithTrees.add("HALLWAY_7");
+		roomsWithTrees.add("HALLWAY_8");
+		roomsWithTrees.add("HALLWAY_9");
+		roomsWithTrees.add("HALLWAY_10");
+		roomsWithTrees.add("HALLWAY_11");
+		roomsWithTrees.add("HALLWAY_12");
+		roomsWithTrees.add("HALLWAY_13");
+		roomsWithTrees.add("HALLWAY_14");
+		roomsWithTrees.add("HALLWAY_15");
+		roomsWithTrees.add("HALLWAY_16");
+		roomsWithTrees.add("HALLWAY_17");
+		roomsWithTrees.add("HALLWAY_18");
+		roomsWithTrees.add("HALLWAY_19");
+		roomsWithTrees.add("HALLWAY_20");
+		roomsWithTrees.add("HALLWAY_21");
+		roomsWithTrees.add("HALLWAY_22");
+		roomsWithTrees.add("HALLWAY_23");
+		roomsWithTrees.add("HALLWAY_24");
+		roomsWithTrees.add("HALLWAY_25");
+		roomsWithTrees.add("HALLWAY_26");
+		roomsWithTrees.add("HALLWAY_27");
+		roomsWithTrees.add("HALLWAY_28");
+		roomsWithTrees.add("HALLWAY_29");
+		roomsWithTrees.add("HALLWAY_30");
+		roomsWithTrees.add("HALLWAY_31");
+
+		// Herbivore enclosures with trees in them
+		roomsWithTrees.add("BRONCHIOSAURUS_NW");
+		roomsWithTrees.add("BRONCHIOSAURUS_NE");
+		roomsWithTrees.add("BRONCHIOSAURUS_SW");
+		roomsWithTrees.add("BRONCHIOSAURUS_SE");
+		
+		roomsWithTrees.add("TRICERATOPS_NW");
+		roomsWithTrees.add("TRICERATOPS_NE");
+		roomsWithTrees.add("TRICERATOPS_SW");
+		roomsWithTrees.add("TRICERATOPS_NE");
+	
+		roomsWithTrees.add("STEGOSAURUS_NW");
+		roomsWithTrees.add("STEGOSAURUS_NE");
+		roomsWithTrees.add("STEGOSAURUS_SW");
+		roomsWithTrees.add("STEGOSAURUS_SE");
+		
+		roomsWithTrees.add("VELOCIRAPTOR_NE");	
+		roomsWithTrees.add("VELOCIRAPTOR_NW");	
+		roomsWithTrees.add("VELOCIRAPTOR_NC");	
+		roomsWithTrees.add("VELOCIRAPTOR_SW");	
+		roomsWithTrees.add("VELOCIRAPTOR_SC");	
+
+		// Specific rooms with trees in them
+		roomsWithTrees.add("EQUIPTMENT_YARD");
+		roomsWithTrees.add("QUARRY_N");
+		roomsWithTrees.add("QUARRY_S");
+		roomsWithTrees.add("MOUNTAIN_TOP");
+		roomsWithTrees.add("PLAINS_E");
+		roomsWithTrees.add("PLAINS_W");	
+		roomsWithTrees.add("HILLS_NW");	
+		roomsWithTrees.add("HILLS_NE");	
+		roomsWithTrees.add("HILLS_SW");	
+		roomsWithTrees.add("HILLS_SE");	
+		roomsWithTrees.add("JUNGLE_NW");	
+		roomsWithTrees.add("JUNGLE_NE");	
+		roomsWithTrees.add("JUNGLE_SW");	
+		roomsWithTrees.add("JUNGLE_SE");	
+		roomsWithTrees.add("FOREST_E");	
+		roomsWithTrees.add("FOREST_W");	
+		roomsWithTrees.add("CABIN");	
+		
+		//Rooms with water 
+		roomsWithWater.add("WATERFALL");
+		roomsWithWater.add("RIVER_2_E");	
+		roomsWithWater.add("RIVER_2_W");	
+		roomsWithWater.add("BEACH_1_N");	
+		roomsWithWater.add("BEACH_1_C");	
+		roomsWithWater.add("BEACH_1_S");	
+		roomsWithWater.add("BEACH_2_N");	
+		roomsWithWater.add("BEACH_2_S");	
+		roomsWithWater.add("LAKE");	
+		roomsWithWater.add("RIVER_1_SE");	
+		roomsWithWater.add("RIVER_1_NE");	
+		roomsWithWater.add("RIVER_1_SW");	
+		roomsWithWater.add("AQUATIC_SW");	
+		roomsWithWater.add("AQUATIC_SE");	
+		roomsWithWater.add("TREX_SC");	
+		roomsWithWater.add("STEGOSAURUS_SW");	
+		roomsWithWater.add("VELOCIRAPTOR_SW");	
+		roomsWithWater.add("SPINOSAURUS_SW	");	
+		roomsWithWater.add("BRONCHIOSAURUS_SE");	
+		roomsWithWater.add("TRICERATOPS_SE");	
+		
+
+
+	}
+}
