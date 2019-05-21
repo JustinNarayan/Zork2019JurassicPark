c369d8e171cf0a60ea664b6a2f5171a51bfd1687
zvelshi <46686793+zvelshi@users.noreply.github.com>
2019-05-21 17:47:46 -0400

    zac eclipse fix

--- JurassicPark/src/com/zork/game/RoomItemInit.java
+++ JurassicPark/src/com/zork/game/RoomItemInit.java
@@ -6,10 +6,10 @@ public class RoomItemInit {
 
 	private ArrayList<String> roomsWithTrees;
 	private ArrayList<String> roomsWithWater;
-	
+
 	public RoomItemInit() {
 		roomsWithTrees = new ArrayList<String>();
-		
+
 		// All hallways/pathways have trees in them
 		roomsWithTrees.add("HALLWAY_1");
 		roomsWithTrees.add("HALLWAY_2");
@@ -48,22 +48,22 @@ public class RoomItemInit {
 		roomsWithTrees.add("BRONCHIOSAURUS_NE");
 		roomsWithTrees.add("BRONCHIOSAURUS_SW");
 		roomsWithTrees.add("BRONCHIOSAURUS_SE");
-		
+
 		roomsWithTrees.add("TRICERATOPS_NW");
 		roomsWithTrees.add("TRICERATOPS_NE");
 		roomsWithTrees.add("TRICERATOPS_SW");
 		roomsWithTrees.add("TRICERATOPS_NE");
-	
+
 		roomsWithTrees.add("STEGOSAURUS_NW");
 		roomsWithTrees.add("STEGOSAURUS_NE");
 		roomsWithTrees.add("STEGOSAURUS_SW");
 		roomsWithTrees.add("STEGOSAURUS_SE");
-		
-		roomsWithTrees.add("VELOCIRAPTOR_NE");	
-		roomsWithTrees.add("VELOCIRAPTOR_NW");	
-		roomsWithTrees.add("VELOCIRAPTOR_NC");	
-		roomsWithTrees.add("VELOCIRAPTOR_SW");	
-		roomsWithTrees.add("VELOCIRAPTOR_SC");	
+
+		roomsWithTrees.add("VELOCIRAPTOR_NE");
+		roomsWithTrees.add("VELOCIRAPTOR_NW");
+		roomsWithTrees.add("VELOCIRAPTOR_NC");
+		roomsWithTrees.add("VELOCIRAPTOR_SW");
+		roomsWithTrees.add("VELOCIRAPTOR_SC");
 
 		// Specific rooms with trees in them
 		roomsWithTrees.add("EQUIPTMENT_YARD");
@@ -71,42 +71,40 @@ public class RoomItemInit {
 		roomsWithTrees.add("QUARRY_S");
 		roomsWithTrees.add("MOUNTAIN_TOP");
 		roomsWithTrees.add("PLAINS_E");
-		roomsWithTrees.add("PLAINS_W");	
-		roomsWithTrees.add("HILLS_NW");	
-		roomsWithTrees.add("HILLS_NE");	
-		roomsWithTrees.add("HILLS_SW");	
-		roomsWithTrees.add("HILLS_SE");	
-		roomsWithTrees.add("JUNGLE_NW");	
-		roomsWithTrees.add("JUNGLE_NE");	
-		roomsWithTrees.add("JUNGLE_SW");	
-		roomsWithTrees.add("JUNGLE_SE");	
-		roomsWithTrees.add("FOREST_E");	
-		roomsWithTrees.add("FOREST_W");	
-		roomsWithTrees.add("CABIN");	
-		
-		//Rooms with water 
-		roomsWithWater.add("WATERFALL");
-		roomsWithWater.add("RIVER_2_E");	
-		roomsWithWater.add("RIVER_2_W");	
-		roomsWithWater.add("BEACH_1_N");	
-		roomsWithWater.add("BEACH_1_C");	
-		roomsWithWater.add("BEACH_1_S");	
-		roomsWithWater.add("BEACH_2_N");	
-		roomsWithWater.add("BEACH_2_S");	
-		roomsWithWater.add("LAKE");	
-		roomsWithWater.add("RIVER_1_SE");	
-		roomsWithWater.add("RIVER_1_NE");	
-		roomsWithWater.add("RIVER_1_SW");	
-		roomsWithWater.add("AQUATIC_SW");	
-		roomsWithWater.add("AQUATIC_SE");	
-		roomsWithWater.add("TREX_SC");	
-		roomsWithWater.add("STEGOSAURUS_SW");	
-		roomsWithWater.add("VELOCIRAPTOR_SW");	
-		roomsWithWater.add("SPINOSAURUS_SW	");	
-		roomsWithWater.add("BRONCHIOSAURUS_SE");	
-		roomsWithWater.add("TRICERATOPS_SE");	
-		
+		roomsWithTrees.add("PLAINS_W");
+		roomsWithTrees.add("HILLS_NW");
+		roomsWithTrees.add("HILLS_NE");
+		roomsWithTrees.add("HILLS_SW");
+		roomsWithTrees.add("HILLS_SE");
+		roomsWithTrees.add("JUNGLE_NW");
+		roomsWithTrees.add("JUNGLE_NE");
+		roomsWithTrees.add("JUNGLE_SW");
+		roomsWithTrees.add("JUNGLE_SE");
+		roomsWithTrees.add("FOREST_E");
+		roomsWithTrees.add("FOREST_W");
+		roomsWithTrees.add("CABIN");
 
+		// Rooms with water
+		roomsWithWater.add("WATERFALL");
+		roomsWithWater.add("RIVER_2_E");
+		roomsWithWater.add("RIVER_2_W");
+		roomsWithWater.add("BEACH_1_N");
+		roomsWithWater.add("BEACH_1_C");
+		roomsWithWater.add("BEACH_1_S");
+		roomsWithWater.add("BEACH_2_N");
+		roomsWithWater.add("BEACH_2_S");
+		roomsWithWater.add("LAKE");
+		roomsWithWater.add("RIVER_1_SE");
+		roomsWithWater.add("RIVER_1_NE");
+		roomsWithWater.add("RIVER_1_SW");
+		roomsWithWater.add("AQUATIC_SW");
+		roomsWithWater.add("AQUATIC_SE");
+		roomsWithWater.add("TREX_SC");
+		roomsWithWater.add("STEGOSAURUS_SW");
+		roomsWithWater.add("VELOCIRAPTOR_SW");
+		roomsWithWater.add("SPINOSAURUS_SW	");
+		roomsWithWater.add("BRONCHIOSAURUS_SE");
+		roomsWithWater.add("TRICERATOPS_SE");
 
 	}
 }
